075442324875d5544371b1a61b22c22fc52fa6c2
James Hsi <hsidori@gmail.com>
2013-09-05 14:33:25 -0700

    change the way node ids are assigned and add classes to the dom nodes

--- js/ids.js
+++ js/ids.js
@@ -6,20 +6,17 @@ define(["storage"], function(storage) {
     if (old_sid) {
         ids.session_id = old_sid;
     } else {
-        ids.session_id = Math.random().toString();
+        ids.session_id = Math.floor(Math.random() * 10000000000000000).toString();
         storage.put('session_id', ids.session_id);
     }
 
     ids.node_ctr = function() {
-        var ctr = parseInt(storage.get('node_ctr') || 0);
+        var ctr = parseInt(storage.get('node_ctr') || 0, 10);
         storage.put('node_ctr', ctr+1);
         return ctr;
     };
 
     ids.node_id = function(document_id) {
-        if (typeof(document_id) == 'undefined') {
-            console.trace();
-        }
         return [ids.session_id, document_id, ids.node_ctr()].join('-');
     };
 

ea484e76ce1457e716c50b7d78229fa0300c7c72
James Hsi <hsidori@gmail.com>
2013-09-30 16:38:11 -0700

    add examples directory with todo app

---
+++ static/examples/todo/main.js
@@ -0,0 +1,86 @@
+$(function () {
+    
+    var createNewTodo = function(newText) {
+        var $newDiv = $("#templates .todo-item").clone();
+        $newDiv.find("label").text(newText);
+        return $newDiv;
+    };
+
+    var updateTodoCount = function (newCount) {
+        var $todoList = $("#todo-list"),
+            numTodos = $todoList.find("li").length - $todoList.find(".completed").length,
+            $todoCount = $("#todo-count");
+        $todoCount.find("strong").text(numTodos);
+        if (numTodos === 1) {
+            $todoCount.html($todoCount.html().replace(/items\s/, "item "));
+        } else {
+            $todoCount.html($todoCount.html().replace(/item\s/, "items "));
+        }
+    };
+
+    var updateClearCompleted = function () {
+        var $btn = $("#clear-completed"),
+            completed_count = $("#todo-list .completed").length;
+
+        if (completed_count > 0) {
+            $btn.removeClass("hidden");
+            $btn.text("Clear completed ("+ completed_count + ")");
+        } else {
+            $btn.addClass("hidden");
+        }
+        updateTodoCount();
+    };
+
+    $("#clear-completed").click(function () {
+        $("#todo-list .completed").remove();
+        $("#clear-completed").addClass("hidden");
+        updateTodoCount();
+    });
+
+    $("#new-todo").keyup(function (evt) {
+        if (evt.keyCode === 13) {
+            var $todos = $("#todo-list"),
+                $textBox = $('#new-todo'),
+                newText = $textBox.val();
+            $todos.prepend(createNewTodo(newText));
+            $textBox.val("");
+
+            updateTodoCount();
+        }
+    });
+
+    $("#todo-list").on("click", ".toggle", function () {
+        var $parent = $(this).parents("li");
+        $parent.toggleClass("completed");
+        updateClearCompleted();
+    });
+
+    $("#todo-list").on("click", ".destroy", function() {
+        $(this).parents("li").remove();
+        updateTodoCount();
+    });
+    
+    $("#filters").on("click", "a", function () {
+        var $filters = $("#filters"),
+            $todos = $("#todo-list"),
+            $this = $(this),
+            selectedFilter = $this.data("filter-name");
+            
+        $filters.find("a").removeClass("selected");
+        $this.addClass("selected");
+         
+        $todos.find("li").removeClass("hidden");
+        if (selectedFilter === "active") {
+            $todos.find("li").filter(function() {
+                return $(this).find('input:checked').length > 0;
+            }).addClass('hidden');
+        } else if (selectedFilter === "completed") {
+            $todos.find("li").filter(function() {
+                return $(this).find('input:checked').length === 0;
+            }).addClass('hidden');
+        } else {}
+
+        updateTodoCount();
+    });
+
+});

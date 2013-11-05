b4d93e7a164cea5a085a4ccd2ba63cc40bb08da4
Bob Poekert <bob@poekert.com>
2013-11-05 15:31:26 -0800

    update test runner script

--- js/tests/run_tests.sh
+++ js/tests/run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd ..
 python ../server/parrot.py &
+server_pid=$!
 python -c 'import time, webbrowser; time.sleep(1); webbrowser.open("http://localhost:8888/tests/unittests.html")' &
 python -m SimpleHTTPServer 8888
+kill $server_pid

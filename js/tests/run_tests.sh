d9cd5d47f4f98ab5d61dd67d18168dd880e5d401
Bob Poekert <bob@poekert.com>
2013-11-04 16:12:48 -0800

    update run_tests.sh

--- js/tests/run_tests.sh
+++ js/tests/run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd ..
+python ../server/parrot.py &
 python -c 'import time, webbrowser; time.sleep(1); webbrowser.open("http://localhost:8888/tests/unittests.html")' &
 python -m SimpleHTTPServer 8888
